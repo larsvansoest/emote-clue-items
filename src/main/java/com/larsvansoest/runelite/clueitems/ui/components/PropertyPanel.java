9e139becae37d3288f2512642d4d13ded6fcea23
Lars van Soest <lars@vansoest.dev>
2021-09-21 16:17:47 +0200

    Even more javadoc3.

--- src/main/java/com/larsvansoest/runelite/clueitems/ui/components/PropertyPanel.java
+++ src/main/java/com/larsvansoest/runelite/clueitems/ui/components/PropertyPanel.java
@@ -18,6 +18,13 @@ public class PropertyPanel extends JPanel
 	private final JLabel nameLabel;
 	private final JLabel valueLabel;
 
+	/**
+	 * Creates the panel.
+	 *
+	 * @param palette Colour scheme for the panel.
+	 * @param name    Name to display before the value.
+	 * @param value   Value to display after the name.
+	 */
 	public PropertyPanel(final EmoteClueItemsPalette palette, final String name, final String value)
 	{
 		super(new GridBagLayout());
@@ -47,11 +54,21 @@ public class PropertyPanel extends JPanel
 		super.add(this.valueLabel, c);
 	}
 
+	/**
+	 * Sets the name of the property panel.
+	 *
+	 * @param name the new name to display before the value.
+	 */
 	public void setName(final String name)
 	{
 		this.nameLabel.setText(String.format("%s:", name));
 	}
 
+	/**
+	 * Sets the value of the property panel.
+	 *
+	 * @param value the new value to display after the name.
+	 */
 	public void setValue(final String value)
 	{
 		this.valueLabel.setText(value.toLowerCase());
